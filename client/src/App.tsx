6dec21a59f97ec6a3a05f586fffbaf69df215e31
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2025-09-30 17:14:14 +0200

    Add mobile css classes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -84,42 +84,48 @@ function App() {
 
 
     return (
-        <div className="flex bg-gray-100 font-poppins h-screen">
-            <div className="flex-grow w-3/4 flex items-center justify-center bg-gray-200">
-                <div className="game-container">
-                    <header className="game-header text-3xl text-center font-extrabold">
+        <div className="flex flex-col sm:flex-row bg-gray-100 font-poppins min-h-screen">
+            <div className="flex-grow w-full sm:w-3/4 flex items-center justify-center bg-gray-200">
+                <div className="game-container w-full max-w-4xl px-2 sm:px-0">
+                    <header className="game-header text-xl sm:text-3xl text-center font-extrabold pt-4">
                         <h1>CMYK Color Mixer</h1>
                     </header>
-                    <section className="goal-section flex justify-around my-4">
-                        <div className="color-display">
-                            <h2 className="text-2xl font-semibold text-center pb-3">Ziel-Farbe</h2>
+                    <section className="goal-section flex flex-row justify-around my-4 gap-6 sm:gap-0">
+                        <div className="color-display flex-1 flex flex-col items-center">
+                            <h2 className="text-md sm:text-2xl font-semibold text-center pb-3">Ziel-Farbe</h2>
                             <div className="color-swatch target-color">
-                                <div className="color-card w-36 h-36 border-2 rounded-xl transition-colors duration-1000 ease-in-out"
+                                <div className="color-card w-28 h-28 sm:w-36 sm:h-36 border-2 rounded-xl text-center
+                                                transition-colors duration-1000 ease-in-out cursor-pointer sm:cursor-default"
                                      style={{
                                          borderColor: `color-mix(in srgb, ${targetColorHex} 100%, black 50%)`,
                                          backgroundColor: targetColorHex
                                      }}
-                                ></div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className={`timer-display flex flex-col items-center cursor-pointer`}
-                                 onClick={() => startNewGame()}
-                            >
-                                <div className={`timer-circle w-24 h-24 border-4 rounded-full bg-white shadow-md
-                                                flex items-center justify-center mt-16 transition-all 
-                                                ${timer <= 0 ? "animate-pulse ring-4 ring-blue-400" : ""}`}
+                                     onClick={() => startNewGame()}
                                 >
-                                    <span className="text-4xl font-bold">
-                                        {timer === -1 ? <StartGameIcon /> : timer}
+                                    <span className="inline-block sm:hidden font-bold text-white text-shadow-test text-xs sm:text-base">
+                                        {timer >= 0 ? timer : ''}
                                     </span>
                                 </div>
                             </div>
                         </div>
-                        <div className="color-display flex flex-col items-center">
-                            <h2 className="text-2xl font-semibold text-center pb-3">Deine Mischung</h2>
+                        <div className="hidden sm:flex flex-1 flex-col items-center">
+                            <div className={`timer-display flex flex-col items-center cursor-pointer`}
+                                 onClick={() => startNewGame()}
+                            >
+                                <div className={`timer-circle w-20 h-20 sm:w-24 sm:h-24 border-4 rounded-full bg-white shadow-md
+                                            flex items-center justify-center mt-8 sm:mt-16 transition-all
+                                            ${timer <= 0 ? "animate-pulse ring-4 ring-blue-400" : ""}`}
+                                >
+                                <span className="text-3xl sm:text-4xl font-bold">
+                                    {timer === -1 ? <StartGameIcon /> : timer}
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="color-display flex-1 flex flex-col items-center">
+                            <h2 className="text-md sm:text-2xl font-semibold text-center pb-3">Deine Mischung</h2>
                             <div className="color-swatch current-mix">
-                                <div className="color-card w-36 h-36 border-2 rounded-xl transition-colors duration-1000 ease-in-out"
+                                <div className="color-card w-28 h-28 sm:w-36 sm:h-36 border-2 rounded-xl transition-colors duration-1000 ease-in-out"
                                      style={{
                                          borderColor: `color-mix(in srgb, ${mixedColorHex} 100%, black 50%)`,
                                          backgroundColor: mixedColorHex
@@ -129,7 +135,7 @@ function App() {
                         </div>
                     </section>
 
-                    <main className="palette-section grid grid-cols-6 gap-4">
+                    <main className="palette-section grid grid-cols-6 sm:grid-cols-6 gap-3 sm:gap-4">
                         {paletteColors.map((c) => {
                             const isSelected = selection.has(c.name);
                             const style = {
@@ -139,13 +145,13 @@ function App() {
                             return (
                                 <div key={c.name}
                                      onClick={() => handleColorSelect([c.name, c.arr])}
-                                     className={`color-card w-30 h-40 rounded-xl flex items-end justify-end
-                                                 cursor-pointer border-2 hover:border-4 transition-transform
-                                                 ${isSelected ? 'scale-105 border-6' : ''}`}
+                                     className={`color-card w-20 h-28 sm:w-30 sm:h-40 rounded-xl flex items-end justify-center sm:justify-end
+                                             cursor-pointer border-2 hover:border-4 transition-transform
+                                             ${isSelected ? 'scale-105 border-6' : ''}`}
                                      style={style}
                                      data-color={c.name}
                                 >
-                                    <div className="card-text p-2 font-bold text-white text-shadow-test">{c.name}</div>
+                                    <div className="card-text p-2 font-bold text-white text-shadow-test text-xs sm:text-base">{c.name}</div>
                                 </div>
                             );
                         })}
@@ -154,7 +160,6 @@ function App() {
                     <footer className="controls-section"></footer>
                 </div>
             </div>
-
             {/* <Chat /> @TODO uncomment when used again */}
         </div>
     );
